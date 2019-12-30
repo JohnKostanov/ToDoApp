38888c37dde25430446d5cda1d62256824d10293
JohnKostanov <kostanovd@icloud.com>
2019-12-30 12:05:51 +0100

    Add class DataProviderTests

--- ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/Controller/DataProvider.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class DataProvider: NSObject {
+    var taskManager: TaskManager?
     
 }
 
 
 extension DataProvider: UITableViewDelegate {
-    
 }
 
 extension DataProvider: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskManager?.tasksCount ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     
 }
